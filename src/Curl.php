e74c07ca64bd015a83499a1aa0f109f9960b2b83
semihaydemir <semih@cloudarena.com>
2017-01-26 16:25:51 +0200

    init

--- src/Curl.php
+++ src/Curl.php
@@ -6,7 +6,7 @@
  * Time: 9:39 AM
  * To change this template use File | Settings | File Templates.
  */
-namespace request;
+namespace assistant;
 class curl {
     private $ch;//Curl Handler
     private $response;
